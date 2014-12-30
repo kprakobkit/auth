f10a46dcfce22f9fe700d241390b38efac5e9679
Peter Prakobkit <kprakobkit@gmail.com>
2014-12-30 15:12:08 -0800

    add users controller test

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, :type => :controller do
+  let(:user_ids) { User.get_ids.to_json }
+  describe "GET index" do
+    before :each do
+      get :index
+    end
+
+    it "should return a json response" do
+      expect(response.header['Content-Type']).to include 'application/json'
+    end
+
+    it "should return the user ids" do
+      expect(response.body).to eq user_ids
+    end
+  end
+end
